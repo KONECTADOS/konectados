e30ad10ceb24d2cfee3c732cf4c8abd0303b3dad
Rafael Andrade <devrafael72@gmail.com>
2021-10-18 19:36:12 -0300

    Transporter

--- src/pages/api/sendemail.ts
+++ src/pages/api/sendemail.ts
@@ -24,14 +24,16 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
   });
   
   
+  
+  
   try {
-    // await transporter.verify(function (error, success) {
-    //   if (error) {
-    //     console.log(error);
-    //   } else {
-    //     console.log("Server is ready to take our messages");
-    //   }
-    // });
+    await transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Server is ready to take our messages");
+      }
+    });
     
     const options = {
       from: `"Konectados" <${process.env.SENDER_ADDRESS}>`, // sender address
